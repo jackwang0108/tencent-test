3b085b141115434b5a14bab02b239adf93fa9bc2
Jack <jack4shihong@gmail.com>
2024-02-24 14:47:10 +0800

    finished test

---
+++ apiTest/debugtalk.py
@@ -0,0 +1,12 @@
+import logging
+import time
+from typing import List
+
+
+# commented out function will be filtered
+# def get_headers():
+#     return {"User-Agent": "hrp"}
+
+
+def get_user_agent():
+    return "hrp/funppy"
