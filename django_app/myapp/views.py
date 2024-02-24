551822a4e7f00a6d4986f39b2676143ce2cc7e62
Jack <jack4shihong@gmail.com>
2024-02-24 13:23:54 +0800

    add django implementation

---
+++ django_app/myapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import JsonResponse
+
+
+def test_view(request):
+    a = request.GET.get("a", None)
+    b = request.GET.get("b", None)
+
+    if b is None:
+        return JsonResponse(
+            {
+                "error_code": "1",
+                "error_message": "Parameter 'b' is required",
+                "reference": "",
+            }
+        )
+
+    response_data = {"error_code": "0", "error_message": "success", "reference": "111"}
+
+    return JsonResponse(response_data)
